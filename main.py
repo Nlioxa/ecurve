f842bc9766d4d6788cdac425996deb91dbe3d7ae
Nlioxa Korzun <36415737+Nlioxa@users.noreply.github.com>
2021-12-03 17:10:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import ecurve as ec
 E = ec.ECurve(p=31, a=-3, b=1)
 
 P = E.point(12, 22)
-Q = E.point(12, 22)
+Q = E.point(13, 19)
+
+R, m = P + Q
+print(f'P**2 = {-R}, m = {m}') # P + Q = (15, 18), m = 28
 
 R, m = P ** 2
-print(f'P**2 = {-R}, m = {m}')
+print(f'P**2 = {-R}, m = {m}') # P ** 2 = (11, 11), m = 2
