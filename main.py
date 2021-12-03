97680475879392c8990808b35c85aed392a6c5a9
Illia Korzun <iluhakorzun@gmail.com>
2021-12-03 17:12:45 +0200

    update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ P = E.point(12, 22)
 Q = E.point(13, 19)
 
 R, m = P + Q
-print(f'P + Q = {-R}, m = {m}') # P + Q = (15, 18), m = 28
+print(f'P + Q\t= {-R}, m = {m}') # P + Q = (15, 18), m = 28
 
 R, m = P ** 2
-print(f'P**2 = {-R}, m = {m}') # P ** 2 = (11, 11), m = 2
+print(f'P ** 2\t= {-R}, m = {m}') # P ** 2 = (11, 11), m = 2
